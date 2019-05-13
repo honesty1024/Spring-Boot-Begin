98299fba68acd31fe09c8c5d4805a248b178a312
unknown <honesty1024@163.com>
2019-05-13 20:22:00 +0800

    first commit

--- src/main/java/com/user/Dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.user.Dao;
-
-import com.user.Module.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-}
